308504dc33028bc66172b494a76407b079dedcfb
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2023-05-14 07:28:35 +0530

    Morning refresh

--- single-post.php
+++ single-post.php
@@ -1,9 +1,33 @@
 <?php
 session_start();
-
-$PageName = " ";
-
 $postID = $_GET['postID'];
+
+if (empty($postID)) {
+    header('Location: ./index.php');
+    exit();
+};
+
+include_once "./connection.php";
+
+$getDataSQLQuery = $connection->prepare("SELECT * FROM allblogs INNER JOIN category ON category.id = allblogs.categoryID INNER JOIN postviews ON postviews.postID = allblogs.postID WHERE allblogs.postID = :postID");
+$getDataSQLQuery->bindValue(":postID", $postID, PDO::PARAM_INT);
+$getDataSQLQuery->execute();
+$allPostData = $getDataSQLQuery->fetch();
+
+$getBlogDataSQLQuery = $connection->prepare("SELECT * FROM blog WHERE postID = :postID");
+$getBlogDataSQLQuery->bindValue(":postID", $postID, PDO::PARAM_INT);
+$getBlogDataSQLQuery->execute();
+if ($getBlogDataSQLQuery->rowCount() > 0) {
+    $allBlogData = $getBlogDataSQLQuery->fetch();
+} else {
+    echo "Not Still Added";
+    exit();
+}
+
+print_r($allPostData);
+$PageName = $allPostData['postTitle'];
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,7 +36,6 @@ $postID = $_GET['postID'];
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- =============== PAGE TITLE =============== -->
     <title>Thamoddya Rashmitha - <?php echo $PageName ?></title>
 
     <?php
@@ -38,15 +61,15 @@ $postID = $_GET['postID'];
     <section class="single-post-wrapper blog-list-wrapper section-padding">
         <div class="container">
             <div class="section-head">
-                <h2 class="heading-secondary">Lifestyle</h2>
+                <h2 class="heading-secondary"><?php echo $allPostData['categoryName'] ?></h2>
                 <ul class="c-bredcrumb">
                     <li>
-                        <a href="#"> Home </a>
+                        <a href="#"> <?php echo $allPostData['tagName'] ?> </a>
                     </li>
                     <!-- tag Name -->
-                    <li><a href="#">Lifestyle</a></li>
+                    <li><a href="#">Single Post</a></li>
                     <!-- Post Title -->
-                    <li><a href="#">Backpacking Essentials for a Weekend Hiking Trip</a></li>
+                    <li><a href="#"><?php echo $allPostData['postTitle'] ?></a></li>
                 </ul>
             </div>
             <div class="row gy-5">
@@ -54,14 +77,21 @@ $postID = $_GET['postID'];
                     <div class="left-content">
                         <!-- Post card -->
                         <div class="post-card">
-                            <div class="card-thumb bg-cover" style="background-image: url('assets/img/single-post/post1.png')"></div>
+                            <div class="card-thumb bg-cover" style="background-image: url('admin/uploads/blogUpload/<?php echo $allPostData['postImage'] ?>')"></div>
                             <div class="card-content">
                                 <div class="card-meta">
                                     <div class="meta-item post-author">
-                                        <span class="category">Travel</span>
-                                        <a href="#" class="author-name">Terence Whittle</a>
+                                        <span class="category"><?php echo $allPostData['tagName'] ?></span>
+                                        <a href="#" class="author-name"><?php echo $allPostData['Author'] ?></a>
                                     </div>
-                                    <span class="meta-item"> Feb 06, 2022 </span>
+                                    <span class="meta-item">
+                                        <?php
+                                        $datetime_str = $allPostData['addedTime'];
+                                        $datetime = DateTime::createFromFormat('Y-m-d H:i:s', $datetime_str);
+                                        $formatted_datetime = $datetime->format('M d, Y');
+                                        echo $formatted_datetime;
+                                        ?>
+                                    </span>
                                     <span class="meta-item">
                                         <i class="fal fa-comment"></i>
                                         13
@@ -74,9 +104,40 @@ $postID = $_GET['postID'];
                                     <div>
 
                                         <h3 class="heading-primary">
-                                            Backpacking Essentials for a Weekend Hiking Trip
+                                            <?php echo $allPostData['postTitle'] ?>
                                         </h3>
 
+
+                                        <p class="body-text">
+                                            Pariatur cupidatat Lorem irure nisi. Velit qui irure consectetur do cupi
+                                            roident id est ex sunt nostrud nisi mine consectetur do cupi roident id
+                                            est ex sunt nostrud nisi minim ut. Cupidatat velit dolore consectetur
+                                            deserunt laboris magna eiusmod aliquip consectetur commodo in eiusmod
+                                            aliqua cupidatat. Nostrud laboris et eu mollit qui esse dolore
+                                            exercitation in dolore sint nisi eu aliqua. Sit ipsum cillum commodo
+                                            enim eiusmod. Eiusmod et anim laborum consectetur. Proident nostrud anim
+                                            nisi sunt veniam. Labore amet eiusmod dolor adipisicing Lorem tempor
+                                            cillum incididunt proident consequat laborum. Velit anim minim est
+                                            consequat commodo eu reprehenderit eiusmod nisi amet quis tempor
+                                            incididunt. Irure proident aute non sit adipisicing nulla sit proident
+                                            dolore sunt. Aute aute Lorem sint sunt et fugiat laborum. Qui incididunt
+                                            aute pariatur aute laborum elit nulla amet magna enim.
+                                        </p>
+                                        <p class="body-text">
+                                            Pariatur cupidatat Lorem irure nisi. Velit qui irure consectetur do cupi
+                                            roident id est ex sunt nostrud nisi mine consectetur do cupi roident id
+                                            est ex sunt nostrud nisi minim ut. Cupidatat velit dolore consectetur
+                                            deserunt laboris magna eiusmod aliquip consectetur commodo in eiusmod
+                                            aliqua cupidatat. Nostrud laboris et eu mollit qui esse dolore
+                                            exercitation in dolore sint nisi eu aliqua. Sit ipsum cillum commodo
+                                            enim eiusmod. Eiusmod et anim laborum consectetur. Proident nostrud anim
+                                            nisi sunt veniam. Labore amet eiusmod dolor adipisicing Lorem tempor
+                                            cillum incididunt proident consequat laborum. Velit anim minim est
+                                            consequat commodo eu reprehenderit eiusmod nisi amet quis tempor
+                                            incididunt. Irure proident aute non sit adipisicing nulla sit proident
+                                            dolore sunt. Aute aute Lorem sint sunt et fugiat laborum. Qui incididunt
+                                            aute pariatur aute laborum elit nulla amet magna enim.
+                                        </p>
                                         <p class="body-text">
                                             Pariatur cupidatat Lorem irure nisi. Velit qui irure consectetur do cupi
                                             roident id est ex sunt nostrud nisi mine consectetur do cupi roident id
@@ -130,13 +191,13 @@ $postID = $_GET['postID'];
                                 <a href="single-post.php"> garden </a>
                                 <a href="single-post.php"> cars </a>
                             </div>
-                        </div> 
+                        </div>
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="right-content">
                         <!-- Widget search box -->
-                        
+
                         <h3 class="heading-tertiary">Recent Posts</h3>
                         <div class="recent-post-card">
                             <div class="card-thumb bg-cover" style="background-image: url('assets/img/recent-post/thumb1.png')"></div>
@@ -172,7 +233,7 @@ $postID = $_GET['postID'];
                                 </a>
                             </div>
                         </div>
-                        
+
                         <!-- Widget add banner
                         <div class="widget-ad-banner bg-cover" style="background-image: url('assets/img/blog/sidebar-add-banner.png')">
                             <div class="content">
@@ -269,7 +330,7 @@ $postID = $_GET['postID'];
         </div>
     </section>
 
- 
+
 
 </body>
 
