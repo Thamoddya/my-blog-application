38a88511c75a594d209f42805f75e41002593edd
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2023-05-15 12:02:16 +0530

    Jquery fix

--- components/head.imports.php
+++ components/head.imports.php
@@ -10,4 +10,3 @@
 
  <!-- ============= MAIN CSS ============= -->
  <link rel="stylesheet" href="assets/css/style.css" />
- <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
