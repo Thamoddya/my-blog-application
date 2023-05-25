f7418ad18f7174c946b2e48b6b0ab9a46a5e0bc7
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2023-05-25 12:07:30 +0530

    edited the banner which makes loo time to load

--- components/smalllAd2.component.php
+++ components/smalllAd2.component.php
@@ -1,4 +1,4 @@
-<div class="widget-ad-banner bg-cover" style="background-image: url('assets/img/blog/sidebar-add-banner.gif')">
+<div class="widget-ad-banner bg-cover" style="background-image: url('assets/img/blog/sidebar-add-banner.png')">
                         <div class="content">
                             <a href="https://thamo.ga/" class="button icon-button active ">
                                 <span>
