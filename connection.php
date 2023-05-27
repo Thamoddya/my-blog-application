d3181daca99dc2d6dd5105480406a8f5f9306e68
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2023-05-27 19:14:19 +0530

    test11

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+// $connection = new mysqli('localhost','root','1234','myblog');
+// $connection = new PDO("mysqli:host=localhost;dbname=myblog", "root", "1234");
+
+$dsn = "mysql:host=localhost;dbname=myblog;charset=utf8mb4";
+$username = "root";
+$password = "1234";
+
+try {
+    $connection = new PDO($dsn, $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error connecting to database: " . $e->getMessage();
+    exit();
+}
+//Check the ignor
