60c8ed629701f7fc167de5a2683eaaa3ed2b78cd
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2023-05-27 20:39:39 +0530

    Update Font

--- single-post.php
+++ single-post.php
@@ -47,6 +47,7 @@ $PageName = $allPostData['postTitle'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $PageName ?></title>
+    <meta name="description" content="<?php echo $PageName ?>">
 
     <?php
     include_once "./components/head.imports.php";
